6b426b4b7f502a016b85412028b5b30a061d723f
ryan <ryan.mporciuncula@gmail.com>
2024-04-02 16:29:40 -0300

    some changes

--- siteclinica/script.js
+++ siteclinica/script.js
@@ -62,3 +62,33 @@ form.addEventListener("submit",(e)=>{
     
 })
 
+function buscarCep(){
+  let cep = document.getElementById("cep").value;
+  if(cep !== ""){
+    let url = "https://brasilapi.com.br/api/cep/v1/" + cep;
+    let req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.send();
+
+      req.onload = function(){
+        if(req.status === 200){
+          let endereco = JSON.parse(req.response);
+          document.getElementById("cidade").value = endereco.city;
+          document.getElementById("rua").value = endereco.street;
+          document.getElementById("bairro").value = endereco.neighborhood;
+        }
+        else if( req.status === 404){
+          span.textContent ="SEU CEP ESTÁ INVÁLIDO"
+        }
+         else{
+           span.textContent = "erro"
+         }
+      }
+  }
+}
+window.addEventListener("click", (e)=>{
+  buscarCep();
+}) 
+
+
+  
