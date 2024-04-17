e4448f8a56aeec767b0829f0e4c36ffd43ba8397
ryan <ryan.mporciuncula@gmail.com>
2024-04-17 15:34:26 -0300

    alterações

--- API SITE FAKE/script.js
+++ API SITE FAKE/script.js
@@ -13,13 +13,15 @@ window.onload = () => {
 }
 
 function buscarProdutos(categoria) {
+
     var url = "https://fakestoreapi.com/products/category/" + categoria
     var request = new XMLHttpRequest();
     request.open("GET", url, false);
     request.send()
     let information = JSON.parse(request.response)
-    console.log(information)
+
     for (let index = 0; index < information.length; index++) {
+
         let image = document.createElement('img')
         let container = document.createElement('div')
         let name = document.createElement('h1')
@@ -34,40 +36,51 @@ function buscarProdutos(categoria) {
         products.style.height = "300px"
         image.id = "imageProduct"
         name.id = "nameProduct"
+
         image.addEventListener("click", (e) => {
+
             modal.style.display = "flex";
             detalhesProdutos.innerHTML = ''
             let imageModal = document.createElement('img')
             let detalhes = document.createElement('p');
             let carrinho = document.createElement('button')
+            let priceModal = document.createElement('h1')
             detalhesProdutos.appendChild(detalhes);
-            detalhesProdutos.appendChild(imageModal)
+            detalhesProdutos.appendChild(imageModal);
+            detalhesProdutos.appendChild(priceModal)
             detalhesProdutos.appendChild(carrinho);
             imageModal.src = information[index].image;
+            priceModal.textContent = "R$" + information[index].price
             imageModal.id = 'imageModal'
             carrinho.innerHTML = "ADICIONAR NO CARRINHO"
             carrinho.id = "botaoCompra"
             detalhes.textContent = information[index].description;
             detalhes.id = "pInModal";
+
             carrinho.addEventListener("click",(e)=>{
+
                 cartModal.style.display = "flex"
                 let container = document.createElement('div');
                 let image = document.createElement('img');
                 let divDescPrice = document.createElement('div');
                 let excluir = document.createElement('button');
                 let x = document.createElement('p')
+                let y = document.createElement('p')
                 cart.appendChild(container)
                 container.appendChild(image)
                 container.appendChild(divDescPrice)
                 container.appendChild(excluir)
+                divDescPrice.appendChild(y)
                 divDescPrice.appendChild(x)
                 image.src = information[index].image
+                y.textContent = information[index].title
                 x.textContent =  "R$ " + information[index].price;
                 image.id = "productInCart"
                 excluir.textContent = "EXCLUIR"
                 excluir.id = "botaoExcluir"
                 container.id = "cartContainer"
                 divDescPrice.id = "divPrice"
+
                 excluir.addEventListener("click",(e)=>{
                     cart.removeChild(container)
                 })
@@ -119,7 +132,9 @@ function listaProdutos() {
     request.open("GET", url, false);
     request.send()
     let information = JSON.parse(request.response)
+
     for (let index = 0; index < information.length; index++) {
+
         let image = document.createElement('img')
         let container = document.createElement('div')
         let name = document.createElement('h1')
@@ -133,40 +148,52 @@ function listaProdutos() {
         price.innerHTML =  "R$ " + information[index].price 
         image.id = "imageProduct"
         name.id = "nameProduct"
+
         image.addEventListener("click", (e) => {
+
             modal.style.display = "flex";
             detalhesProdutos.innerHTML = ''
             let detalhes = document.createElement('p');
             let carrinho = document.createElement('button')
             let imageModal = document.createElement('img')
+            let priceModal = document.createElement('h1')
             detalhesProdutos.appendChild(detalhes);
             detalhesProdutos.appendChild(imageModal)
+            detalhesProdutos.appendChild(priceModal)
             detalhesProdutos.appendChild(carrinho)
             imageModal.src = information[index].image;
+            priceModal.textContent = "R$" + information[index].price
             imageModal.id = 'imageModal'
             carrinho.innerHTML = "ADICIONAR NO CARRINHO"
             carrinho.id = "botaoCompra"
             detalhes.textContent = information[index].description;
             detalhes.id = "pInModal";
+
             carrinho.addEventListener("click",(e)=>{
+
                 cartModal.style.display = "flex"
                 let container = document.createElement('div');
                 let image = document.createElement('img');
                 let divDescPrice = document.createElement('div');
                 let excluir = document.createElement('button');
                 let x = document.createElement('p')
+                let y = document.createElement('p')
                 cart.appendChild(container)
                 container.appendChild(image)
                 container.appendChild(divDescPrice)
                 container.appendChild(excluir)
+                divDescPrice.appendChild(y)
                 divDescPrice.appendChild(x)
                 image.src = information[index].image
                 x.textContent =  "R$ " + information[index].price;
+                y.textContent = information[index].title
+                container.id = "cartContainer"
                 image.id = "productInCart"
                 excluir.textContent = "EXCLUIR"
                 excluir.id = "botaoExcluir"
                 container.id = "cartContainer"
                 divDescPrice.id = "divPrice"
+                y.id = "cartProductName"
                 excluir.addEventListener("click",(e)=>{
                     cart.removeChild(container)
                 })
@@ -179,3 +206,4 @@ function listaProdutos() {
 close.addEventListener("click",(e)=>{
     modal.style.display = "none"
 })
+
