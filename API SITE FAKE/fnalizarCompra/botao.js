387c180e03687670d1a75df307c6d261d0b4b2c2
ryan <ryan.mporciuncula@gmail.com>
2024-04-23 17:42:49 -0300

    changes

--- API SITE FAKE/fnalizarCompra/botao.js
+++ API SITE FAKE/fnalizarCompra/botao.js
@@ -1,15 +1,47 @@
 const btnavancar1 = document.querySelector("#proximoPessoal");
-const finalizar = document.querySelector("#proximoTransporte")
+const finalizar = document.querySelector("#proximoTransporte");
+const input = document.querySelectorAll(".enabled");
+const span = document.querySelector("#span");
+const finilizarInput = document.querySelectorAll(".disabled");
+const spanFinalizar = document.querySelector("#spanFinalizar");
+const titulo = document.querySelector("#titulo")
 
 btnavancar1.addEventListener("click", (e)=>{
-    infoPessoal.style.pointerEvents = "none";
-    infoPessoal.style.opacity = "0.4"
-    transporte.style.pointerEvents = "All"
-    transporte.style.opacity = "1"
+    if (input[0].name === "NOME" && input[0].value === ""){
+        span.textContent = "PREENCHA SEU NOME"
+  
+    }else if(input[1].name === "EMAIL" && input[1].value === "" ){
+        span.textContent = "PREENCHA SEU EMAIL"
+  
+    }else if(input[2].name === "TELEFONE" && input[2].value === "" ){
+      span.textContent = "PREENCHA SEU TELEFONE"
+    }else{
+        span.textContent = ""
+        infoPessoal.style.pointerEvents = "none";
+        infoPessoal.style.opacity = "0.4"
+        transporte.style.pointerEvents = "All"
+        transporte.style.opacity = "1"
+    }
+    
 })
 
-// finalizar.addEventListener("click", (e)=>{
-//     transporte.style.pointerEvents = "none"
-//     transporte.style.opacity = "0.4"
-//     totalCompra.innerHTML = "PARABÉNS, VOCÊ COMPROU SEU PRODUTO"
-// })
+
+finalizar.addEventListener("click", (e)=>{
+    if (finilizarInput[0].name === "CEP" && finilizarInput[0].value === ""){
+        spanFinalizar.textContent = "PREENCHA SEU CEP"
+  
+    }else if(finilizarInput[1].name === "CIDADE" && finilizarInput[1].value === "" ){
+        spanFinalizar.textContent = "PREENCHA SUA CIDADE"
+  
+    }else if(finilizarInput[2].name === "BAIRRO" && finilizarInput[2].value === "" ){
+        spanFinalizar.textContent = "PREENCHA SEU BAIRRO"
+
+    }else if(finilizarInput[3].name === "RUA" && finilizarInput[3].value === "" ){
+        spanFinalizar.textContent = "PREENCHA SUA RUA"
+    }else{
+        titulo.textContent = "PARABÉNS VOCÊ COMPROU SEU PRODUTO"
+        transporte.style.pointerEvents = "none"
+        transporte.style.opacity = "0.4"
+        
+    }
+})
