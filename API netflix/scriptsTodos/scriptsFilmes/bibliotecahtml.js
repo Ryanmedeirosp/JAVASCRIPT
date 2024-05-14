f799e0cbd3d61c5c2981ae6651c3ac66b7c4a60f
ryan <ryan.mporciuncula@gmail.com>
2024-05-14 11:32:31 -0300

    changes

--- API netflix/scriptsTodos/scriptsFilmes/bibliotecahtml.js
+++ API netflix/scriptsTodos/scriptsFilmes/bibliotecahtml.js
@@ -1,13 +1,11 @@
+const main = document.querySelector("#main");
+const body = document.querySelector("#body");
+const divFilmes = document.querySelector("#divFilmes");
+
 const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTcyYTRkOGE4ZDIxYzc2NmZjY2RjYmIxMjM2Nzc5NSIsInN1YiI6IjY2Mzk1YjZlMmZhZjRkMDEyN2M2OTcwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xX0faRcrdR9wa2dkqLio5qCbi4QFWGYX0ixkI00JGNs'
-    }
-  };
-  
-fetch('https://api.themoviedb.org/3/movie/popular?language=pt-br&page=1', options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err));
-
+}
+};
---
+++ API netflix/scriptsTodos/scriptsSeries/bibliotecaHtml.js
@@ -0,0 +1,11 @@
+const main = document.querySelector("#main");
+const body = document.querySelector("#body");
+const divFilmes = document.querySelector("#divFilmes");
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTcyYTRkOGE4ZDIxYzc2NmZjY2RjYmIxMjM2Nzc5NSIsInN1YiI6IjY2Mzk1YjZlMmZhZjRkMDEyN2M2OTcwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xX0faRcrdR9wa2dkqLio5qCbi4QFWGYX0ixkI00JGNs'
+}
+};
